f127501793a009c214b1e981c3c079f7b32f94b0
princee1 <89865089+princee1@users.noreply.github.com>
2021-10-14 19:33:18 -0400

    restart

--- src/tp2/HashMap.java
+++ src/tp2/HashMap.java
@@ -1,12 +1,14 @@
 package tp2;
 
+import java.security.KeyStore;
+
 public class HashMap<KeyType, DataType> {
 
     private static final int DEFAULT_CAPACITY = 20;
     private static final float DEFAULT_LOAD_FACTOR = 0.5f;
     private static final int CAPACITY_INCREASE_FACTOR = 2;
 
-    private Node<KeyType, DataType>[] map;
+    public Node<KeyType, DataType>[] map;
     private int size = 0;
     private int capacity;
     private final float loadFactor; // Compression factor
@@ -14,19 +16,22 @@ public class HashMap<KeyType, DataType> {
     /**
      * Constructeur par défaut
      */
-    public HashMap() { this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR); }
+    public HashMap() {
+        this(DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR);
+    }
 
     /**
      * Constructeur par parametre
+     * 
      * @param initialCapacity
      */
     public HashMap(int initialCapacity) {
-        this(initialCapacity > 0 ? initialCapacity : DEFAULT_CAPACITY,
-                DEFAULT_LOAD_FACTOR);
+        this(initialCapacity > 0 ? initialCapacity : DEFAULT_CAPACITY, DEFAULT_LOAD_FACTOR);
     }
 
     /**
      * Constructeur par parametres
+     * 
      * @param initialCapacity
      * @param loadFactor
      */
@@ -37,12 +42,14 @@ public class HashMap<KeyType, DataType> {
     }
 
     /**
-     * Finds the index attached to a particular key
-     * This is the hashing function ("Fonction de dispersement")
-     * @param key Value used to access to a particular instance of a DataType within map
+     * Finds the index attached to a particular key This is the hashing function
+     * ("Fonction de dispersement")
+     * 
+     * @param key Value used to access to a particular instance of a DataType within
+     *            map
      * @return Index value where this key should be placed in attribute map
      */
-    private int hash(KeyType key){
+    public int hash(KeyType key) {
         int keyHash = key.hashCode() % capacity;
         return Math.abs(keyHash);
     }
@@ -64,7 +71,9 @@ public class HashMap<KeyType, DataType> {
     /**
      * @return Current reserved space for the map
      */
-    public int capacity(){ return capacity; }
+    public int capacity() {
+        return capacity;
+    }
 
     /**
      * @return if map does not contain any element
@@ -73,54 +82,116 @@ public class HashMap<KeyType, DataType> {
         return size == 0;
     }
 
-    /** TODO:
-     * Increases capacity by CAPACITY_INCREASE_FACTOR (multiplication) and
+    /**
+     * TODO: Increases capacity by CAPACITY_INCREASE_FACTOR (multiplication) and
      * reassigns all contained values within the new map
      */
     private void rehash() {
+        Node<KeyType, DataType> oldArray[] = map;
+        this.capacity = HashMap.CAPACITY_INCREASE_FACTOR * oldArray.length;
+        map = new Node[this.capacity];
+
+        for (int i = 0; i < oldArray.length; i++) {
+            map[i] = oldArray[i];
+        }
+
         return;
     }
 
-    /** TODO:
-     * Finds if map contains a key
+    /**
+     * TODO: Finds if map contains a key
+     * 
      * @param key Key which we want to know if exists within map
      * @return if key is already used in map
      */
     public boolean containsKey(KeyType key) {
-        return false;
+        return getCurrentNode(key) != null;
     }
 
-    /** TODO:
-     * Finds the value attached to a key
+    /**
+     * TODO: Finds the value attached to a key
+     * 
      * @param key Key which we want to have its value
      * @return DataType instance attached to key (null if not found)
      */
     public DataType get(KeyType key) {
-        return null;
+
+        try {
+            return getCurrentNode(key).data;
+        } catch (NullPointerException e) {
+            return null;
+        }
     }
 
-    /**TODO:
-     * Assigns a value to a key
+    /**
+     * TODO: Assigns a value to a key
+     * 
      * @param key Key which will have its value assigned or reassigned
      * @return Old DataType instance at key (null if none existed)
      */
     public DataType put(KeyType key, DataType value) {
-        return null;
+        DataType oldData = null;
+        int hashKey = hash(key);
+        Node<KeyType, DataType> currentNode = map[hashKey];
+        if (currentNode != null) {
+            while (!currentNode.key.equals(key)) {
+                if (currentNode.next == null) {
+                    currentNode.next = new Node<KeyType, DataType>(key, value);
+                    size++;
+                    if (needRehash())
+                        rehash();
+                    return null;
+                }
+                currentNode = currentNode.next;
+            }
+            oldData = currentNode.data;
+            currentNode.data = value;
+        } else {
+            map[hashKey] = new Node<KeyType, DataType>(key, value);
+            size++;
+            if (needRehash())
+                rehash();
+
+        }
+
+        return oldData;
     }
 
-    /**TODO:
-     * Removes the node attached to a key
+    /**
+     * TODO: Removes the node attached to a key
+     * 
      * @param key Key which is contained in the node to remove
      * @return Old DataType instance at key (null if none existed)
      */
     public DataType remove(KeyType key) {
+
         return null;
     }
 
-    /**TODO:
-     * Removes all nodes contained within the map
+    /**
+     * TODO: Removes all nodes contained within the map
      */
     public void clear() {
+        for (int i = 0; i < this.capacity; i++) {
+            map[i] = null;
+        }
+        this.size = 0;
+    }
+
+    private Node<KeyType, DataType> getCurrentNode(KeyType key) {
+        Node<KeyType, DataType> currentNode = map[hash(key)];
+
+
+        while (currentNode != null && !currentNode.key.equals(key)) {
+
+            System.out.println(currentNode.data);
+            currentNode = currentNode.next;
+
+            if (currentNode == null)
+                break;
+        }
+
+        return currentNode;
     }
 
     /**
@@ -131,8 +202,7 @@ public class HashMap<KeyType, DataType> {
         DataType data;
         Node<KeyType, DataType> next; // Pointer to the next node within a Linked List
 
-        Node(KeyType key, DataType data)
-        {
+        Node(KeyType key, DataType data) {
             this.key = key;
             this.data = data;
             next = null;
